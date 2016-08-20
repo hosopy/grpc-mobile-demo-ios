d0f7ccd4968a75ee218ed10e7dfd85a6d14b2dc8
hosopy <dev.hosopy@gmail.com>
2016-08-20 15:34:34 +0900

    first commit

---
+++ GrpcMobileDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GrpcMobileDemo
+//
+//  Created by Keishi Hosoba on 2016/08/20.
+//  Copyright © 2016年 hosopy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
