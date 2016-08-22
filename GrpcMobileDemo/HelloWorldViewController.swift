5e3a1f1adbaad24570238814d0d2675b259e77d6
hosopy <dev.hosopy@gmail.com>
2016-08-22 20:08:24 +0900

    define HelloWorldViewController

---
+++ GrpcMobileDemo/HelloWorldViewController.swift
@@ -0,0 +1,25 @@
+//
+//  HelloWorldViewController.swift
+//  GrpcMobileDemo
+//
+//  Created by Keishi Hosoba on 2016/08/22.
+//  Copyright © 2016年 hosopy. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class HelloWorldViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
